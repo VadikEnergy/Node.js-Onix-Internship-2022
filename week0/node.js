4d6940d73f8953d191da146fddc4a613c94e189d
Vadik <43725331+VadikEnergy@users.noreply.github.com>
2022-11-09 20:19:02 +0200

    Add files via upload
    optimized some code

--- week0/node.js
+++ week0/node.js
@@ -2,20 +2,18 @@
  * 1. call https://jsonplaceholder.typicode.com/users and write it to file users.json
  * todo: install module to call this API, and use node FS module
  */
- const request = require('request'); // * install module to call API
- const fs = require("fs");
+const request = require('request'); // * install module to call API
+const fs = require("fs");
 
- request('https://jsonplaceholder.typicode.com/users', { json: true }, (err, res, body) => {
-
-   if (err) { return console.log(err); }
-   fs.writeFile("users.json", JSON.stringify(body), err => {
-
-    if (err) { return console.log(err); }
-    console.log("New file 'users.json' successfully done writing"); // Success
+function fetchAndWriteFile(url, fileName){
+  request(`${url}`, { json: true }, (err, res, body) => {
+    fs.writeFile(`${fileName}`, JSON.stringify(body), err => {
+      console.log(`New file '${fileName}' successfully done writing`); // Success
+    });
   });
+}
 
- });
-
+fetchAndWriteFile('https://jsonplaceholder.typicode.com/users','users.json');
 
 /**
  * 2. Let's work with running node script with some environment variables
@@ -23,39 +21,8 @@
  * If param is PRODUCTION  get data from https://jsonplaceholder.typicode.com/todos and write it to file todos.json
  * If param is DEV get data from https://jsonplaceholder.typicode.com/albums and write if to file albums.json
  */
-
 require('dotenv').config();
 
-getJsonFile(process.env.ENV);
-
-function getJsonFile(param) {
-
-  if (param === 'PRODUCTION') {
-
-    request('https://jsonplaceholder.typicode.com/todos', { json: true }, (err, res, body) => {
-  
-     if (err) { return console.log(err); }
-     fs.writeFile("todos.json", JSON.stringify(body), err => {
-  
-      if (err) { return console.log(err); }
-      console.log("New file 'todos.json' successfully done writing"); // Success
-    });
-  
-   });
-  } else if (param === 'DEV') {
-  
-    request('https://jsonplaceholder.typicode.com/albums', { json: true }, (err, res, body) => {
-  
-     if (err) { return console.log(err); }
-     fs.writeFile("albums.json", JSON.stringify(body), err => {
-  
-      if (err) { return console.log(err); }
-      console.log("New file 'albums.json' successfully done writing"); // Success
-    });
-  
-   });
-  } else {
-    console.log(' The new param is not a PRODUCTION or DEV ');
-  }
-
-}
+const config =  process.env.ENV === 'PRODUCTION' ? fetchAndWriteFile('https://jsonplaceholder.typicode.com/todos','todos.json') :
+                process.env.ENV === 'DEV' ? fetchAndWriteFile('https://jsonplaceholder.typicode.com/albums','albums.json'):
+                console.log(' The new param is not a PRODUCTION or DEV ');
